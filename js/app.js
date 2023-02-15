847fc3669af3ebdf2e4b0fa10b6653449a32cea9
Roberto Carriero <roberto_carriero1@yahoo.com.ar>
2023-02-15 01:08:56 -0300

    First Version

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const mostrarReloj =()=>{
+    let fecha = new Date;
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+    document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    const dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sábado'];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let año =fecha.getFullYear();
+    let fechaTexto = `${diaSemana}, ${dia} ${mes} ${año}`;
+    document.getElementById('fecha').innerHTML = fechaTexto;
+}
+
+/* Para agregar el digito 0 cuando la hora, min, seg tengan un digito*/
+const formatoHora = (hora)=>{
+    if( hora < 10)
+         hora = '0'+ hora;
+    return hora;
+}
+
+
+setInterval(mostrarReloj, 1000);/* Con setInterval delamos pasar 1 seg con la leyenda Relo Digital*/
+
+
+
